8a8adf21ef429cdfe02be2f51562f4d661f7bda7
Andrew Walker <154334+ardwalker@users.noreply.github.com>
2025-10-02 11:28:10 +0100

    DC-7863: update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.14.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.19.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"         % bootstrapVersion,
