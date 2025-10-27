0b705bd5752bb69c319decbf73e3cb0a8bb95445
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2025-10-27 17:37:08 +0000

    DC-7937 Upgrade dependencies for mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val hmrcMongoVersion = "2.10.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"         % bootstrapVersion,
